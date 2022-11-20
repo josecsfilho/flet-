ebb9259b3bb3512d662f1bb155289ff2f896f51e
José Filho <100032235+josecsfilho@users.noreply.github.com>
2022-11-20 20:09:58 -0300

    Add files via upload

--- Flet-portifolio/main.py
+++ Flet-portifolio/main.py
@@ -1,5 +1,8 @@
 # #13547a
 # #80d0c7
+# #0f172a s-9
+# #475569 s-6
+
 
 """" Flet Responsivel Portifolio - Website """
 
@@ -19,6 +22,11 @@ from flet import(
     LinearGradient,
     PopupMenuButton,
     PopupMenuItem,
+    icons,
+    Icon,
+    IconButton,
+    animation,
+    transform,
 
 )
 
@@ -26,10 +34,15 @@ def main(page: Page):
     # title
     page.title = "Flet Portfolio"
 
-
-
+    #
+    def _animate_social(e):
+        if e.data == "true":
+            _icon_text_.offset, _icon_text_.opacity = transform.Offset(0, 0.05), 0
+            _icon_text_.update()
     #
     def on_resize(e):
+        _background.control.height = page.height
+
         if page.width <= 730:
             _nav.controls[0].visible = False
             _nav.update()
@@ -43,11 +56,11 @@ def main(page: Page):
 
     #
     def _change_text_color(e):
-        if e.control.content.color == 'black':
-            e.control.content.color = "white70"
+        if e.control.content.color == "white":
+            e.control.content.color = "#475569"
             e.control.content.update()
         else:
-            e.control.content.color = "black"
+            e.control.content.color = "white"
             e.control.content.update()
 
     # navbar
@@ -60,37 +73,34 @@ def main(page: Page):
                 content=Row(
                     controls=[
                         Container(
-                            on_hover=lambda e:
-                            _change_text_color(e),
+                            on_hover=lambda e: _change_text_color(e),
                             content=Text(
                                 "Sobre",
                                 weight="w600",
-                                color="black",
+                                color="white",
                             ),
                         ),
                         Container(
-                            on_hover=lambda e:
-                            _change_text_color(e),
-                             content=Text(
+                            on_hover=lambda e: _change_text_color(e),
+                            content=Text(
                                 "Contato",
                                 weight="w600",
-                                color="black",
+                                color="white",
                             ),
                         ),
                         Container(
-                            on_hover=lambda e:
-                            _change_text_color(e),
+                            on_hover=lambda e: _change_text_color(e),
                             content=Text(
                                 "Serviços",
                                 weight="w600",
-                                color="black",
+                                color="white",
                             ),
                         ),
                     ]
                 ),
             ),
         ],
-    )
+    )                                                                            
 
     # minimizando navbar
     _min_nav = Row(
@@ -106,7 +116,6 @@ def main(page: Page):
         ],
     )
 
-
     #título
     _title = ResponsiveRow(
         alignment="center",
@@ -116,8 +125,7 @@ def main(page: Page):
                 alignment=alignment.top_center,
                 padding=20,
                 content=Text(
-                    "Framework Flet ",
-                    color="white",
+                    "Framework Flet ", color="white", # Foi inserido cor do título.
                     size=45,
                     weight="w600",
                     text_align="center",
@@ -126,16 +134,89 @@ def main(page: Page):
         ],
     )
 
+    # títilo heading
+    _sub_title_ = ResponsiveRow(
+        alignment="center",
+        controls=[
+            Container(
+                col={"xs": 12, "sm": 10, "md": 10, "lg": 10, "xl": 12},
+                padding=20, 
+                alignment=alignment.top_center,
+                content=Text(
+                    "Seja bem vindo a minha Webpage pessoal.", color="white",
+                    text_align="center",
+                    size=16,
+                    weight="w500",
+                ),
+            )
+        ],
+    )
+
+    # botão midias sociais
+    _icon_list_ = [icons.FACEBOOK, icons.SHARE_SHARP, icons.TIKTOK_SHARP]
+
+    _social_button = Row(        
+        alignment="center",
+        vertical_alignment="center",        
+    )
+
+    _icon_text_ = Text(
+        "Connect!",
+        size=16,
+        color="white",
+        weight="w800",
+        animate_opacity=500,
+        offset=transform.Offset(0, -1.1),
+        animate_offset=animation.Animation(duration=1000, curve="elasticOut"),
+    )
+    
+    for icon in _icon_list_:
+        _icon = IconButton(
+            icon=icon,
+            icon_size=22,
+            icon_color="white",
+            offset=transform.Offset(0, -0.9),
+            animate_offset=animation.Animation(duration=1000, curve="elasticOut"),
+            animate_opacity=200,
+            opacity=0,
+        )
+        _social_button.controls.append(_icon)
+
+    _icon_container = Container(
+        width=145, 
+        height=50, 
+        bgcolor="blue800",
+        border_radius=8, #
+        alignment=alignment.center,        
+        on_hover=lambda e: _animate_social(e),
+        content=Column(
+            spacing=0, 
+            alignment="center",
+            horizontal_alignment="center",       
+            controls=[
+                _social_button,
+                Row(
+                    alignment="center",
+                    controls=[
+                        _icon_text_,
+                    ],
+                ),
+            ],
+        ),
+    )
 
     # main column
     _main_col = Column(horizontal_alignment="center")
     _main_col.controls.append(_nav)
     _main_col.controls.append(_min_nav)
     _main_col.controls.append(_title)
+    _main_col.controls.append(_sub_title_)
+    _main_col.controls.append(_icon_container)
 
     # bg container
     _background = Container(
         height=page.height,
+        expand=True,
         margin=-10,
         gradient=LinearGradient(
             begin=alignment.bottom_left,
